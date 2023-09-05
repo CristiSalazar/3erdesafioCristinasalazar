fb8352ef69f05aab1baf4309676d4119274bd408
CristiSalazar <cristina.salazar125@gmail.com>
2023-09-05 00:04:30 -0400

    2do commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
-const http = require("http")
+//  const http = require("http")
 
-const server = http.createServer((req, res) => {
-    res.end("hola")
+//  const server = http.createServer((req, res) => {
+//       res.end("hola")
+//   })
+
+//   server.listen(8080, () => {
+//       console.log("servidor escuchando")
+//   })
+
+const express = require("express")
+const app = express()
+const PORT = 8080
+
+const usuarios = [
+    {id: "1", nombre:"Javier", apellido:"Jimenez", edad: "20"},
+    {id: "2", nombre:"Matias", apellido:"Jimenez", edad: "22"}
+]
+
+app.get("/", (req,res) => {
+    res.send(usuarios)
 })
 
-server.listen(8080, () => {
-    console.log("servidor escuchando")
+app.get("/:idUsuario", (req, res) => {
+    let idUsuario = req.params.idUsuario
+    let usuario = usuarios.find(u => u.id === idUsuario)
+    res.send({usuario})
+})
+
+app.listen(PORT, () => {
+    console.log("server escuchando")
 })
